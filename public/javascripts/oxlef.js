0c714bef6d754c765b347e7251f9866c37fe58c0
Jackson Lo <7jackson.lo@gmail.com>
2016-05-09 19:44:04 -0400

    Added highcharts view data on selection

--- public/javascripts/oxlef.js
+++ public/javascripts/oxlef.js
@@ -1,6 +1,75 @@
 //
 $(document).ready(function() {
 	$("#menu").on("click", ".item", function() {
-		
+		var team = $(this).attr("data-value");
+		$.get("/get_team_stats",
+		{
+			team: team,
+		},
+		function(data, status){
+			console.log(data);
+		});
+
+		$.get("/get_team_data",
+		{
+			team: team,
+		},
+		function(data, status){
+			//console.log(data);
+			$('#chart').highcharts({
+				chart: {
+					zoomType: 'x'
+				},
+				title: {
+					text: team + "'s Market Capitalization"
+				},
+				subtitle: {
+					text: 'Overall Market Sticker Value Over Time'
+				},
+				xAxis: {
+					type: 'datetime'
+				},
+				yAxis: {
+					title: {
+						text: 'Market Capitalization'
+					}
+				},
+				legend: {
+					enabled: false
+				},
+				plotOptions: {
+					area: {
+						fillColor: {
+							linearGradient: {
+								x1: 0,
+								y1: 0,
+								x2: 0,
+								y2: 1
+							},
+							stops: [
+								[0, Highcharts.getOptions().colors[0]],
+								[1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+							]
+						},
+						marker: {
+							radius: 2
+						},
+						lineWidth: 1,
+						states: {
+							hover: {
+								lineWidth: 1
+							}
+						},
+						threshold: null
+					}
+				},
+
+				series: [{
+					type: 'area',
+					name: 'Market Capitalization',
+					data: data
+				}]
+			});
+		});
 	});
 });
--- routes/index.js
+++ routes/index.js
@@ -12,4 +12,18 @@ router.get('/', function(req, res, next) {
 	});
 });
 
+router.get('/get_team_stats', function(req, res, next) {
+	var team = req.query.team;
+	var results = db.get().collection(collection_name).find({"name": team}).toArray(function(err, docs) {
+		res.send(docs);
+	});
+});
+
+router.get('/get_team_data', function(req, res, next) {
+	var team = req.query.team;
+	var results = db.get().collection(collection_name).find({"team": team}).toArray(function(err, docs) {
+		res.send(docs);
+	});
+});
+
 module.exports = router;
